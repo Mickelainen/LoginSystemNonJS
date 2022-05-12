380bc39194b15b7929250b371d908ee634486391
Mickelainen <mikaelgustafsson315@gmail.com>
2022-05-12 13:11:08 +0200

    Add project files.

---
+++ LoginSystemNonJS/Database/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LoginSystemNonJS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginSystemNonJS.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> users { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { 
+        }
+    }
+}
